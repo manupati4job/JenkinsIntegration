b694bc8bafb9b8811ab665ff6d90e7e1d8c021bf
itsmy <itsmy@DESKTOP-K591F3O>
2020-01-27 14:30:13 -0500

    my first commit today
    Change-Id: Ibcc056c1c045ccd8dfb331fcf8f527382332c672

---
+++ JenkinsIntegration/JenkinsIntegration/src/test/java/JenkinsIntegration/JenkinsIntegration/AppTest.java
@@ -0,0 +1,66 @@
+
+
+
+package JenkinsIntegration.JenkinsIntegration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+@Test
+public class AppTest {
+
+    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String driverPath = "C:\\Users\\itsmy\\Desktop\\Jan 2019 Job Interview\\SeleniumProject\\Drivers\\chromedriver.exe";
+    public WebDriver driver ;  
+	ChromeOptions options = new ChromeOptions();
+	
+    @BeforeTest
+    public void launchBrowser() {
+        System.out.println("launching Chome browser"); 
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+    }
+    
+    @BeforeMethod
+    public void verifyHomepageTitle() {
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    
+    @Test(priority = 0)
+ public void register(){
+     driver.findElement(By.linkText("REGISTER")).click() ;
+     String expected = "Register: Mercury Tours";
+     String actual = driver.getTitle();
+     Assert.assertEquals(actual, expected);
+ }
+     @Test(priority = 1)
+ public void support() {
+       driver.findElement(By.linkText("SUPPORT")).click() ;
+       String expected = "Under Construction: Mercury Tours";
+       String actual = driver.getTitle();
+       Assert.assertEquals(actual, expected);
+ }
+     
+     @AfterMethod
+     public void goBackToHomepage ( ) {
+           driver.findElement(By.linkText("Home")).click() ;
+     }
+     
+    @AfterTest
+    public void terminateBrowser(){
+        driver.close();
+    }
+	
+	
+  
+}
